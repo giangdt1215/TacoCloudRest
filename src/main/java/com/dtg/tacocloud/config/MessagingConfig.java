dc17ef8d1894b4721944c7d1247573f0eac824e9
GiangDT <giangdt1215@gmail.com>
2022-10-01 22:01:15 +0700

    Add MessageConverter bean

--- src/main/java/com/dtg/tacocloud/config/MessagingConfig.java
+++ src/main/java/com/dtg/tacocloud/config/MessagingConfig.java
@@ -1,12 +1,15 @@
 package com.dtg.tacocloud.config;
 
+import com.dtg.tacocloud.model.TacoOrder;
 import org.apache.activemq.artemis.jms.client.ActiveMQDestination;
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 
 import javax.jms.Destination;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class MessagingConfig {
@@ -14,5 +17,17 @@ public class MessagingConfig {
     @Bean
     public Destination orderQueue(){
         return new ActiveMQQueue("tacocloud.order.queue");
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter messageConverter(){
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("_typeId");
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("order", TacoOrder.class);
+        messageConverter.setTypeIdMappings(typeIdMappings);
+
+        return messageConverter;
     }
 }
